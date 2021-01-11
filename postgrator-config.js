c9286d493401f136ac9aa2a2fac212715bbf3c91
Andrew Schmit <schmita91@gmail.com>
2021-01-11 14:54:42 -0500

    migrations

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,9 +1,15 @@
+const { CONNECTION_STRING } = require("./src/config");
 
-require('dotenv').config();
+require('dotenv', { CONNECTION_STRING }).config();
 
 module.exports = {
   migrationDirectory: 'migrations',
   driver: 'pg',
+  host: CONNECTION_STRING.host,
+  port: CONNECTION_STRING.port,
+  dbname: CONNECTION_STRING.dbname,
+  user: CONNECTION_STRING.user,
+  password: CONNECTION_STRING.password,
   connectionString:
     process.env.NODE_ENV === 'test'
       ? process.env.TEST_DATABASE_URL
