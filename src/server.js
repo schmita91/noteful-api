47a62616a9a60201bf3ef14b43dc2b6ce82ae684
Andrew Schmit <schmita91@gmail.com>
2021-01-11 11:37:18 -0500

    initial commit

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
-const app = require('./app')
-const { PORT } = require('./config')
+const knex = require('knex');
 
-app.listen(PORT, () => {
+const app = require('./app'),
+  { PORT, DB_URL } = require('./config');
+
+const db = knex({
+  client: 'pg',
+  connection: DB_URL,
+});
+
+app.set('db', db);
+
+app.listen(PORT, () =>
   console.log(`Server listening at http://localhost:${PORT}`)
-})
+);
